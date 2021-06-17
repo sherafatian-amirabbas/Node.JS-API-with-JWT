2099c388a64d0b4b8adb719beaf70a31f1b585e0
dev-services <dev-services@testlio.com>
2021-06-17 00:20:49 -0400

    task4-completed

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 const Koa = require('koa');
 const router = require('./lib/routes');
+const session = require('./lib/session');
 
 const app = new Koa();
 const PORT = 8080;
 
 app.use(require('koa-bodyparser')());
+
+app.use(async (context, next) => { 
+    context.session = session(context);
+    await next(); 
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,8 +1,9 @@
 const koaJwt = require('koa-jwt');
 
-const secret_key = 'secret_key'; // TODO: should be as env variable
-
-module.exports = {
-    secret: secret_key,
-    jwt: koaJwt({secret: secret_key})
-};
+module.exports = function() {
+    const secret_key = 'secret_key'; // TODO: should be as env variable
+    return {
+        secret: secret_key,
+        jwt: koaJwt({secret: secret_key})
+    };
+}();
